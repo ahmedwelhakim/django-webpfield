bbcd99b1e0a17f1cb5b33b311935c5279315666c
Ahmed Elhakim <ahmed.w.elhakim@gmail.com>
2023-06-21 17:22:11 +0300

    Working in tests

---
+++ tests/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from tests.models import TestImageModel
+
+admin.site.register(TestImageModel)
--- tests/tests.py
+++ tests/tests.py
@@ -2,8 +2,10 @@ import io
 import os
 from shutil import rmtree
 
+from django.contrib.auth.models import User
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.test import TestCase
+from django.urls import reverse
 from PIL import Image, ImageChops
 
 from webpfield.webp_storage import WebPStorage
@@ -13,13 +15,20 @@ from .utils import override_webpfield_settings
 
 
 class TestBase(TestCase):
-    def setUp(self):
-        self.jpg_path = os.path.join(MEDIA_ROOT, "python-logo.jpg")
-        self.png_path = os.path.join(MEDIA_ROOT, "python-logo.png")
-        self.webp_path = os.path.join(MEDIA_ROOT, "python-logo.webp")
-        self.gif_path = os.path.join(MEDIA_ROOT, "python-logo.gif")
+    jpg_path = None
+    png_path = None
+    webp_path = None
+    gif_path = None
+    storage = None
 
-        self.storage = WebPStorage()
+    @classmethod
+    def setUp(cls):
+        cls.jpg_path = os.path.join(MEDIA_ROOT, "python-logo.jpg")
+        cls.png_path = os.path.join(MEDIA_ROOT, "python-logo.png")
+        cls.webp_path = os.path.join(MEDIA_ROOT, "python-logo.webp")
+        cls.gif_path = os.path.join(MEDIA_ROOT, "python-logo.gif")
+
+        cls.storage = WebPStorage()
 
     def tearDown(self):
         try:
@@ -35,60 +44,10 @@ class TestBase(TestCase):
     def path_to_name(path):
         return os.path.basename(path)
 
-
-class WebPStorageTest(TestBase):
-    @override_webpfield_settings(DELETE_ORIGINAL=False)
-    def test_save_jpg_to_webp_image_with_no_delete(self):
-        file_name, file = self._get_image_file(self.jpg_path, "image/jpeg", "JPEG")
-        saved_name = self.storage.save(file_name, file)
-        # Check that the file was saved as a WebP image
-        self.assertTrue(saved_name.endswith(".webp"))
-        # Check original image exist
-        self.assertTrue(self.storage.exists(file_name))
-
-    @override_webpfield_settings(DELETE_ORIGINAL=True)
-    def test_save_jpg_to_webp_image_with_delete(self):
-        file_name, file = self._get_image_file(self.jpg_path, "image/jpeg", "JPEG")
-        saved_name = self.storage.save(file_name, file)
-        # Check that the file was saved as a WebP image
-        self.assertTrue(saved_name.endswith(".webp"))
-        # Check original image does not exist
-        self.assertFalse(self.storage.exists(file_name))
-
-    @override_webpfield_settings(DELETE_ORIGINAL=False)
-    def test_save_png_to_webp_image_with_no_delete(self):
-        file_name, file = self._get_image_file(self.png_path, "image/png", "PNG")
-        saved_name = self.storage.save(file_name, file)
-        # Check that the file was saved as a WebP image
-        self.assertTrue(saved_name.endswith(".webp"))
-        # Check original image exist
-        self.assertTrue(self.storage.exists(file_name))
-
-    @override_webpfield_settings(DELETE_ORIGINAL=True)
-    def test_save_png_to_webp_image_with_delete(self):
-        file_name, file = self._get_image_file(self.png_path, "image/png", "PNG")
-        saved_name = self.storage.save(file_name, file)
-        # Check that the file was saved as a WebP image
-        self.assertTrue(saved_name.endswith(".webp"))
-        # Check original image does not exist
-        self.assertFalse(self.storage.exists(file_name))
-
-    def test_save_webp_is_same_as_original(self):
-        file_name, file = self._get_image_file(self.webp_path, "image/webp", "WEBP")
-        saved_name = self.storage.save(file_name, file)
-        self.assertTrue(
-            os.path.basename(saved_name) == self.path_to_name(self.webp_path)
-        )
-
-        image1 = Image.open(self.webp_path)
-        image2 = Image.open(file_name)
-
-        diff = ImageChops.difference(image1, image2)
-        self.assertTrue(diff.getbbox() is None)
-
-    def _get_image_file(self, path, content_type, img_format):
+    @classmethod
+    def _get_image_file(cls, path, content_type, img_format):
         img = Image.open(path)
-        file_name = self.path(self.path_to_name(path))
+        file_name = cls.path(cls.path_to_name(path))
         content_type = content_type
         bytes_io = io.BytesIO()
         img.save(bytes_io, img_format)
@@ -102,3 +61,85 @@ class WebPStorageTest(TestBase):
             None,
         )
         return file_name, file
+
+    @classmethod
+    def get_jpg_image(cls):
+        return cls._get_image_file(cls.jpg_path, "image/jpeg", "JPEG")
+
+    @classmethod
+    def get_png_image(cls):
+        return cls._get_image_file(cls.png_path, "image/png", "PNG")
+
+    @classmethod
+    def get_webp_image(cls):
+        return cls._get_image_file(cls.webp_path, "image/webp", "WEBP")
+
+
+class WebPStorageTest(TestBase):
+    @override_webpfield_settings(DELETE_ORIGINAL=False)
+    def test_save_jpg_to_webp_image_with_no_delete(self):
+        file_name, file = self.get_jpg_image()
+        saved_name = self.storage.save(file_name, file)
+        # Check that the file was saved as a WebP image
+        self.assertTrue(saved_name.endswith(".webp"))
+        # Check original image exist
+        self.assertTrue(self.storage.exists(file_name))
+
+    @override_webpfield_settings(DELETE_ORIGINAL=True)
+    def test_save_jpg_to_webp_image_with_delete(self):
+        file_name, file = self.get_jpg_image()
+        saved_name = self.storage.save(file_name, file)
+        # Check that the file was saved as a WebP image
+        self.assertTrue(saved_name.endswith(".webp"))
+        # Check original image does not exist
+        self.assertFalse(self.storage.exists(file_name))
+
+    @override_webpfield_settings(DELETE_ORIGINAL=False)
+    def test_save_png_to_webp_image_with_no_delete(self):
+        file_name, file = self.get_png_image()
+        saved_name = self.storage.save(file_name, file)
+        # Check that the file was saved as a WebP image
+        self.assertTrue(saved_name.endswith(".webp"))
+        # Check original image exist
+        self.assertTrue(self.storage.exists(file_name))
+
+    @override_webpfield_settings(DELETE_ORIGINAL=True)
+    def test_save_png_to_webp_image_with_delete(self):
+        file_name, file = self.get_png_image()
+        saved_name = self.storage.save(file_name, file)
+        # Check that the file was saved as a WebP image
+        self.assertTrue(saved_name.endswith(".webp"))
+        # Check original image does not exist
+        self.assertFalse(self.storage.exists(file_name))
+
+    def test_save_webp_is_same_as_original(self):
+        file_name, file = self.get_webp_image()
+        saved_name = self.storage.save(file_name, file)
+        self.assertTrue(
+            os.path.basename(saved_name) == self.path_to_name(self.webp_path)
+        )
+
+        image1 = Image.open(self.webp_path)
+        image2 = Image.open(file_name)
+
+        diff = ImageChops.difference(image1, image2)
+        self.assertTrue(diff.getbbox() is None)
+
+
+class AdminImageUploadTestCase(TestBase):
+    def setUp(self):
+        super().setUp()
+        self.admin_user = User.objects.create_superuser(
+            "admin", "admin@example.com", "password"
+        )
+        self.client.login(username="admin", password="password")
+
+    def test_admin_upload_image(self):
+        url = reverse("admin:tests_testimagemodel_add")
+        data = {
+            "name": "Test Image",
+            "image": self.get_jpg_image(),
+        }
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 302)
